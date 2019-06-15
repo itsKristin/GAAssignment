e3fb858a7fe72b08cbde7630e1c066fbb7396912
Kristin Stock <kristin_stock@icloud.com>
2019-06-15 12:17:46 +0200

    Adding Movementsystem

---
+++ Assets/Task III/Scripts/Systems/MovementSystem.cs
@@ -0,0 +1,49 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+
+public class MovementSystem : JobComponentSystem
+{
+    // This declares a new kind of job, which is a unit of work to do.
+    // The job is declared as an IJobForEach<Translation, Rotation>,
+    // meaning it will process all entities in the world that have both
+    // Translation and Rotation components. Change it to process the component
+    // types you want.
+    //
+    // The job is also tagged with the BurstCompile attribute, which means
+    // that the Burst compiler will optimize it for the best performance.
+    [BurstCompile]
+    struct MovementSystemJob : IJobForEach<Translation, CustomRigidbody, Velocity>
+    {
+        // Add fields here that your job needs to do its work.
+        // For example,
+        public float deltaTime;
+        
+        
+        
+        public void Execute(ref Translation _translation, [ReadOnly] ref CustomRigidbody _customrigidbody, ref Velocity _velocity)
+        {   
+            _translation.Value += _velocity.Value * deltaTime;
+               
+        }
+    }
+    
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        var job = new MovementSystemJob();
+        
+        // Assign values to the fields on your job here, so that it has
+        // everything it needs to do its work when it runs later.
+        // For example,
+        job.deltaTime = UnityEngine.Time.deltaTime;
+        
+        
+        
+        // Now that the job is set up, schedule it to be run. 
+        return job.Schedule(this, inputDependencies);
+    }
+}
