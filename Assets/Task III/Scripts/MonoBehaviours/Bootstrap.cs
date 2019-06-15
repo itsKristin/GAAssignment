9f2f6f5bbfcf43cc023137bd38450dd58a383bec
Kristin Stock <kristin_stock@icloud.com>
2019-06-15 12:16:07 +0200

    Adding Basic UI

---
+++ Assets/Task III/Scripts/MonoBehaviours/Bootstrap.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+using Unity.Entities;
+
+public class Bootstrap : MonoBehaviour
+{
+    private float3 spaceshipDimensions = new float3(1f,1f,1f);
+    private float spaceshipMass;
+
+    public Material spaceshipMaterial;
+    public bool Gravity;
+
+    public float3 SpaceshipDimensions { get { return spaceshipDimensions; } set { spaceshipDimensions = value; } }
+    public float SpaceshipMass { get { return spaceshipMass; } set { spaceshipMass = value; } }
+    public GameObject SpaceshipPrefab { get { return spaceshipPrefab; } set { spaceshipPrefab = value; } }
+
+    private EntityManager entityManager;
+    private GameObject spaceshipPrefab;
+
+    private void Start()
+    {
+        entityManager = World.Active.EntityManager;
+
+        spaceshipPrefab = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        spaceshipPrefab.GetComponent<MeshRenderer>().material = spaceshipMaterial;
+        spaceshipPrefab.transform.position = Vector3.zero;
+        spaceshipPrefab.transform.localScale = spaceshipDimensions;
+        spaceshipPrefab.tag = "Cube";
+    }
+
+    public void RunSimulation()
+    {
+        Entity spaceshipEntity = GameObjectConversionUtility.ConvertGameObjectHierarchy(spaceshipPrefab, World.Active);
+        Destroy(spaceshipPrefab);
+
+        entityManager.AddComponentData(spaceshipEntity, new SpaceshipbodyComponent { Dimensions = spaceshipDimensions, Mass = spaceshipMass });
+        entityManager.AddComponentData(spaceshipEntity, new CustomRigidbody { });
+        entityManager.AddComponentData(spaceshipEntity, new Velocity { Value = new float3(0f, 0f, 0f) });
+        entityManager.AddComponentData(spaceshipEntity, new Gravity { Value = Gravity ? new float3(0f, -9.8f, 0f) : new float3(0f, 0f, 0f) });
+    }
+
+    public void Refresh()
+    {
+        spaceshipPrefab.transform.localScale = spaceshipDimensions;
+    }
+    
+
+}
---
+++ Assets/Task III/Scripts/MonoBehaviours/SpaceshipMotionUI.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Mathematics;
+
+public class SpaceshipMotionUI : MonoBehaviour
+{
+    public GameObject SettingsUI;
+    public GameObject SimulationUI;
+
+    public InputField Length;
+    public InputField Width;
+    public InputField Height;
+
+    public Button RunSimulation;
+    public Button PlaceThruster;
+
+    public Bootstrap Bootstrapper;
+    public ThrusterPlacement Thrusterplacement;
+
+    private void Awake()
+    {
+        Length.contentType = InputField.ContentType.DecimalNumber;
+        Length.onValueChanged.AddListener(delegate
+        {
+            float potentialValue = (Length.text == null || Length.text == string.Empty) ? 0f : float.Parse(Length.text);
+        
+            Bootstrapper.SpaceshipDimensions = new float3
+            {
+                x = potentialValue,
+                y = Bootstrapper.SpaceshipDimensions.y,
+                z = Bootstrapper.SpaceshipDimensions.z
+            };
+            Bootstrapper.Refresh();
+        });
+
+        Width.contentType = InputField.ContentType.DecimalNumber;
+        Width.onValueChanged.AddListener(delegate
+        {
+            float potentialValue = (Width.text == null || Width.text == string.Empty) ? 0f : float.Parse(Width.text);
+            
+            Bootstrapper.SpaceshipDimensions = new float3
+            {
+                x = Bootstrapper.SpaceshipDimensions.x,
+                y = potentialValue,
+                z = Bootstrapper.SpaceshipDimensions.z
+            };
+            Bootstrapper.Refresh();
+        });
+
+        Height.contentType = InputField.ContentType.DecimalNumber;
+        Height.onValueChanged.AddListener(delegate
+        {
+            float potentialValue = (Height.text == null || Height.text == string.Empty) ? 0f : float.Parse(Height.text);
+            
+            Bootstrapper.SpaceshipDimensions = new float3
+            {
+                x = Bootstrapper.SpaceshipDimensions.x,
+                y = Bootstrapper.SpaceshipDimensions.y,
+                z = potentialValue
+            };
+            Bootstrapper.Refresh();
+            
+        });
+
+        RunSimulation.onClick.AddListener(() => 
+        {
+            Bootstrapper.RunSimulation();
+            SettingsUI.SetActive(false);
+            //SimulationUI.SetActive(true);
+        });
+
+
+        PlaceThruster.onClick.AddListener(() => 
+        {
+            Thrusterplacement.PlaceThruster = true;
+        });
+
+    }
+
+    
+    
+
+
+}
