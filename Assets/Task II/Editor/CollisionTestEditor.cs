dfc1a334dec6356c17e1a33445b211be003ec217
Kristin Stock <kristin_stock@icloud.com>
2019-06-17 15:08:35 +0200

    Adding Editor for Inspector Buttons

---
+++ Assets/Task II/Editor/CollisionTestEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor (typeof (CollisionTester))]
+public class CollisionTestEditor : Editor {
+
+    public override void OnInspectorGUI () {
+        DrawDefaultInspector ();
+
+        if (GUILayout.Button ("Test Collision")) {
+            var tester = ((CollisionTester) target);
+            tester.TestCollision ();
+        }
+
+        if (GUILayout.Button ("Reset")) {
+            var tester = ((CollisionTester) target);
+            tester.Reset();
+        }
+    }
+}
